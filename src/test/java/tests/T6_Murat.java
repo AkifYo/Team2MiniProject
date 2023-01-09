58ed9a734391a9aa7466cab8cc640750cc252ac5
akif <70863898+akif-ned@users.noreply.github.com>
2023-01-09 23:17:52 +0100

    first commit

---
+++ src/test/java/tests/T6_Murat.java
@@ -0,0 +1,15 @@
+package tests;
+
+public class T6_Murat {
+    //github instructions
+    //1- projeyi github uzerinden pull ediniz
+    //2- projeyi intelij de actiktdan sonra kendi branchinizi olusturunuz.
+    //3- kendi branciniz uzerinde asagida yer alan testleri gerceklestiriniz.
+    //4- test pass olduktan sonra kodunuzu github a push ediniz
+    //testler
+    //1-Qualitydemy anasayfasina gidiniz.
+    //2-Arama kutusunu locate ediniz.
+    //3-"Language" kelimesini aratiniz.
+    //4-Cikan sonuclar arasinda language kelimesinin gectigini test ediniz.
+    //5-Cikan butun kurslarin textlerini alarak yazdiriniz
+}
