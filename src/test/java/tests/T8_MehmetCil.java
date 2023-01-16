2a6b9c548fcad504f6b58a85d866803021dfc324
Mehmetcil <mehmetcil096@gmail.com>
2023-01-17 00:35:34 +0300

    17.01.23

--- src/test/java/tests/T8_MehmetCil.java
+++ src/test/java/tests/T8_MehmetCil.java
@@ -1,12 +1,35 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.testng.IConfigurable;
+import org.testng.annotations.Test;
+import org.testng.internal.IConfiguration;
+import pages.QualityDemyPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class T8_MehmetCil {
+
     //github instructions
     //1- projeyi github uzerinden pull ediniz
     //2- projeyi intelij de actiktdan sonra kendi branchinizi olusturunuz.
     //3- kendi branciniz uzerinde asagida yer alan testleri gerceklestiriniz.
     //4- test pass olduktan sonra kodunuzu github a push ediniz
     //testler
-    //1-Qualitydemy anasayfasina gidiniz.
-    //2-Ana sayfada oldugunuzu test ediniz
+
+    @Test
+    public void test01(){
+
+        // 1-Qualitydemy anasayfasina gidiniz.
+        Driver.getdriver().get(ConfigReader.getProperty("qdUrl"));
+
+        // 2-Ana sayfada login girisini test ediniz
+        QualityDemyPage qualityDemyPage = new QualityDemyPage();
+        qualityDemyPage.ilkLoginLinki.click();
+
+        // 3-Kullanici girisi test ediniz
+        qualityDemyPage.kullaniciEmailKutusu.sendKeys("kabeg86625@themesw.com");
+        qualityDemyPage.passwordKutusu.sendKeys("Asdfg123+" + Keys.ENTER);
+    }
 }
