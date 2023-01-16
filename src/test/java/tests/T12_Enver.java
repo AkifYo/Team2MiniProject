8fbdedd66641d182f428fb4bcf4f1a2e3001ea2d
nvrymk <enverymk@gmail.com>
2023-01-16 18:01:04 +0200

    16.01.2023-Enver

---
+++ src/test/java/tests/T12_Enver.java
@@ -0,0 +1,24 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import pages.QualityDemyPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class T12_Enver {
+
+    public static void main(String[] args) {
+        System.out.println("daawdaaw" +
+                "faeffea");
+    }
+
+
+    }
+    //6- ilgili kursunu sepette olup olmadigini test ediniz.
+
+
