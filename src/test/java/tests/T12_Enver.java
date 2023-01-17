305d6a9a7d365c10dc404cfb57ce875c6509265a
nvrymk <enverymk@gmail.com>
2023-01-17 15:25:07 +0200

    16.01.2023-Enver

--- src/test/java/tests/T12_Enver.java
+++ src/test/java/tests/T12_Enver.java
@@ -11,7 +11,9 @@ import utilities.Driver;
 import utilities.ReusableMethods;
 
 public class T12_Enver {
-
+    public static void main(String[] args) {
+        System.out.println("artık bu sonnn");
+    }
 
 
 
