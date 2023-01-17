7a0d2ee975fec7e32dddbbe059c640bb9c29a359
nvrymk <enverymk@gmail.com>
2023-01-17 14:42:27 +0200

    Merge pull request #14 from AkifYo/enver
    Enver

--- src/test/java/tests/T12_Enver.java
+++ src/test/java/tests/T12_Enver.java
@@ -2,7 +2,7 @@ package tests;
 
 public class T12_Enver {
     public static void main(String[] args) {
-        System.out.println("T12_Enver buralardan gecmek uzere 222");
+        System.out.println("T12_Enver buralardan gecmek uzere 22D2");
     }
 }
 //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
