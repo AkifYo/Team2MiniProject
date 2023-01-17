2a42b48cd52fb9e64d191ae8d9e7743708f0dbbf
Mehmetcil <mehmetcil096@gmail.com>
2023-01-17 13:46:34 +0300

    17.01.23 M

--- src/test/java/tests/T8_MehmetCil.java
+++ src/test/java/tests/T8_MehmetCil.java
@@ -32,4 +32,11 @@ public class T8_MehmetCil {
         qualityDemyPage.kullaniciEmailKutusu.sendKeys("kabeg86625@themesw.com");
         qualityDemyPage.passwordKutusu.sendKeys("Asdfg123+" + Keys.ENTER);
     }
+
+    //github instructions
+    //1- projeyi github uzerinden pull ediniz
+    //2- projeyi intelij de actiktdan sonra kendi branchinizi olusturunuz.
+    //3- kendi branciniz uzerinde asagida yer alan testleri gerceklestiriniz.
+    //4- test pass olduktan sonra kodunuzu github a push ediniz
+    //testler
 }
