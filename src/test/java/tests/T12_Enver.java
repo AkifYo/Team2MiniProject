7f31ec038aee8faa02db93f0c4b177a94213d934
nvrymk <enverymk@gmail.com>
2023-01-17 14:45:27 +0200

    push pull request #14 from AkifYo/enver
    Enver

--- src/test/java/tests/T12_Enver.java
+++ src/test/java/tests/T12_Enver.java
@@ -3,6 +3,7 @@ package tests;
 public class T12_Enver {
     public static void main(String[] args) {
         System.out.println("T12_Enver buralardan gecmek uzere 22D2");
+        System.out.println("bakalım bu nasıl olacakddawdaw??????");
     }
 }
 //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
